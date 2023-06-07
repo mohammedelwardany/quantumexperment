6ae2b18847edef0ae158aa478cf1b817ca8e73d4
rhazem13 <rhazem13@yahoo.com>
2023-06-07 17:05:35 +0300

    fix footer icons , logo responsiviness

--- src/source/components/footer/index.js
+++ src/source/components/footer/index.js
@@ -1,26 +1,33 @@
-
-import { Layout, Space } from 'antd';
-import { Col, Row } from 'antd';
-import "./style.css"
-import { InboxOutlined, PhoneOutlined, TwitterOutlined, FacebookOutlined, LinkedinOutlined } from '@ant-design/icons';
-
+import { Layout, Space } from "antd";
+import { Col, Row } from "antd";
+import "./style.css";
+import {
+  InboxOutlined,
+  PhoneOutlined,
+  TwitterOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 
 const { Footer } = Layout;
-
 
 const FooterComponent = () => {
   //function u can use here
   return (
-
-    <Footer style={{ textAlign: 'start', backgroundColor: "#000", color: "#fff" }}>
-
-
-
-
-      <Row  style={{display:"flex",justifyContent:"start"}} className='alighmentScreens'>
+    <Footer
+      style={{ textAlign: "start", backgroundColor: "#000", color: "#fff" }}
+    >
+      <Row
+        style={{ display: "flex", justifyContent: "start" }}
+        className="alighmentScreens"
+      >
         <Col sm={24} md={24} lg={3} xl={3}></Col>
-        <Col sm={24} md={24} lg={4} xl={4} >
-           <img src={require('../../../assets/BrandLogo.png')} style={{width:"100%",paddingInline:"5vh"}}/>
+        <Col sm={24} md={24} lg={4} xl={4}>
+          <img
+            src={require("../../../assets/BrandLogo.png")}
+            style={{ paddingInline: "5vh" }}
+            className="footer-logo"
+          />
           <br />
           {/*<Space size={'small'}><InboxOutlined style={{ fontSize: '25px' }} />
             Quantum@mail.com </Space>
@@ -28,60 +35,128 @@ const FooterComponent = () => {
           <Space size={'small'}>
             <PhoneOutlined style={{ fontSize: '25px' }} />   +20 1060404916 </Space>
           <br />*/}
-          <div style={{paddingTop:"1vh",fontSize:"2vh"}}>
-            <p className='font-gilroy' style={{display:"flex",alignItems:"center",justifyContent:"center"}}>you can also follow us at </p>
+          <div style={{ paddingTop: "1vh", fontSize: "2vh" }}>
+            <p
+              className="font-gilroy"
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              you can also follow us at{" "}
+            </p>
           </div>
-          
-          <Row  style={{display:"flex",alignItems:"center",justifyContent:"space-around",fontSize:"2vh" }}>
 
-            <Col md={8} lg={8} xl={8} style={{display:"flex",alignItems:"center",justifyContent:"center"}} >
+          <Row
+          className="icon-row"
+           
+          >
+            <Col
+              md={8}
+              lg={8}
+              xl={8}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               {/* <i className="fa-brands fa-linkedin-in" ></i> */}
-              <i class="fa-brands fa-facebook-f" style={{fontSize:"3.5vh"}}></i>
+              <i
+                class="fa-brands fa-facebook-f"
+                style={{ fontSize: "3.5vh" }}
+              ></i>
             </Col>
-            <Col md={8} lg={8} xl={8} style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <Col
+              md={8}
+              lg={8}
+              xl={8}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               {/* <LinkedinOutlined style={{ fontSize:"3vh" }} /> */}
-              <i className="fa-brands fa-linkedin-in" style={{fontSize:"4vh"}}></i>
+              <i
+                className="fa-brands fa-linkedin-in"
+                style={{ fontSize: "4vh" }}
+              ></i>
             </Col>
-            <Col md={8} lg={8} xl={8} style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-            <i class="fa-brands fa-twitter" style={{ fontSize:"4vh" }}></i>
+            <Col
+              md={8}
+              lg={8}
+              xl={8}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <i class="fa-brands fa-twitter" style={{ fontSize: "4vh" }}></i>
               {/* <TwitterOutlined  /> */}
             </Col>
-          </Row> 
-
+          </Row>
         </Col>
         <Col sm={24} md={24} lg={4} xl={4}></Col>
-        <Col sm={24} md={24} lg={4} xl={4} style={{display:"flex",flexDirection:"column",fontSize:"2vh"}} className='alighmentScreens'>  <h3 className='headerfooter'>LET US HELP YOU</h3>
-        <div style={{paddingTop:"2vh"}} className='alighmentjustfyScreens'>
-          <div>
-                  <Space style={{marginBlock:"1vh"}} size={'small'} className='font-gilroy'>
-                    {/* <InboxOutlined  /> */}
-                    <i class="fa-solid fa-inbox" style={{ fontSize:"3vh" , marginInlineEnd:"1vh"}}></i>
-                  Info@quantumme.net </Space>
+        <Col
+          sm={24}
+          md={24}
+          lg={4}
+          xl={4}
+          style={{ display: "flex", flexDirection: "column", fontSize: "2vh" }}
+          className="alighmentScreens"
+        >
+          {" "}
+          <h3 className="headerfooter">LET US HELP YOU</h3>
+          <div style={{ paddingTop: "2vh" }} className="alighmentjustfyScreens">
+            <div>
+              <Space
+                style={{ marginBlock: "1vh" }}
+                size={"small"}
+                className="font-gilroy"
+              >
+                {/* <InboxOutlined  /> */}
+                <i
+                  class="fa-solid fa-inbox"
+                  style={{ fontSize: "3vh", marginInlineEnd: "1vh" }}
+                ></i>
+                Info@quantumme.net{" "}
+              </Space>
 
-          <Space size={'small'} >
-            <i class="fa-solid fa-phone" style={{ fontSize:"3vh" , marginInlineEnd:"1vh" }}></i>   +971 56 715 8226 </Space>
-        </div>
-        </div>
+              <Space size={"small"}>
+                <i
+                  class="fa-solid fa-phone"
+                  style={{ fontSize: "3vh", marginInlineEnd: "1vh" }}
+                ></i>{" "}
+                +971 56 715 8226{" "}
+              </Space>
+            </div>
+          </div>
         </Col>
         <Col sm={24} md={24} lg={4} xl={4}></Col>
-        <Col sm={24} md={24} lg={4} xl={4} style={{display:"flex",flexDirection:"column",fontSize:"2vh"}} className='alighmentScreens'><h3 className='headerfooter'>VISIT QUANTUM</h3>
-        <div style={{marginBlock:"1vh"}}>
-        <p>Shams Business Center, Sharjah Media City free Zone, Al Messaned, Sharjah, UAE.</p>
-        </div>
-        
+        <Col
+          sm={24}
+          md={24}
+          lg={4}
+          xl={4}
+          style={{ display: "flex", flexDirection: "column", fontSize: "2vh" }}
+          className="alighmentScreens"
+        >
+          <h3 className="headerfooter">VISIT QUANTUM</h3>
+          <div style={{ marginBlock: "1vh" }}>
+            <p>
+              Shams Business Center, Sharjah Media City free Zone, Al Messaned,
+              Sharjah, UAE.
+            </p>
+          </div>
         </Col>
 
         <Col sm={24} md={24} lg={2} xl={2}></Col>
-
-
       </Row>
-
-
-
     </Footer>
-
-
   );
-}
+};
 
 export { FooterComponent };
